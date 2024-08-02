bb196d9ae5c78be91f0f599bf94a7490c95489ad
Angus529 <n11301996@qut.edu.au>
2024-08-02 17:18:36 +1000

    Authentication

--- AuthenticationService.java
+++ AuthenticationService.java
@@ -9,13 +9,20 @@ public class AuthenticationService implements IAuthenticationService {
 
     // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
 
-    public void signUp() {
-
+    public User signUp(String username, String password) {
+        for (int i = 0; i < users.length; i++) {
+            if (username == users.get(i))
+                return null;
+        }
+        return users.add(username);
     }
 
     // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
 
     public void logIn() {
-
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i))
+                if (password == users.get(i))
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Main {
     // ... (other methods and fields)
 
@@ -7,7 +9,7 @@ class Main {
         String username = scanner.nextLine();
         System.out.print("Enter your password: ");
         String password = scanner.nextLine();
-        User user = authService.logIn(username, password);
+        User user = AuthenticationService.logIn(username, password);
         System.out.println("Welcome, " + user.getUsername() + "!");
         // TODO Now: Create an instance of the ToDoList class with the logged-in user and call the run method
     }
